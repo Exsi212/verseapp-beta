4d18551530c285d104a39d4fbd1f5f31353937c9
Olya Aleshina <39920514+Exsi212@users.noreply.github.com>
2024-02-05 09:38:43 +0300

    Add files via upload

---
+++ verse/verse/verseApp.swift
@@ -0,0 +1,17 @@
+//
+//  verseApp.swift
+//  verse
+//
+//  Created by Olya on 17.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct verseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
